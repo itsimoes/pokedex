81f4c13866af6120ee4ec74ac1194442a1dc49e2
itsimoes <2d.italo@gmail.com>
2022-12-01 15:33:04 -0300

    inicializando navbar e pokemonCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+import Navbar from "../components/Navbar";
+import PokemonCard from "../components/PokemonCard";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Container maxWidth="false">
+        <Grid container>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+          <Grid item xs={3}>
+            <PokemonCard />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
