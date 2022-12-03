d520c7b64ca7ad6363e8e3b2d4efbea6c6df817e
itsimoes <2d.italo@gmail.com>
2022-12-03 17:11:08 -0300

    Component PokemonList criado e código da Home mudado para ele

---
+++ src/components/PokemonList/index.jsx
@@ -0,0 +1,64 @@
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+import Navbar from "../../components/Navbar";
+import PokemonCard from "../../components/PokemonCard";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Skeletons } from "../../components/Skeletons";
+
+const PokemonList = () => {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  const getPokemons = () => {
+    var endpoints = [];
+    for (var i = 1; i < 151; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    }
+    axios
+      .all(endpoints.map((endpoint) => axios.get(endpoint)))
+      .then((res) => setPokemons(res))
+      .catch((err) => console.log(err));
+  };
+
+  const pokemonFilter = (name) => {
+    var filteredPokemons = [];
+    if (name === "") {
+      getPokemons();
+    }
+    for (var i in pokemons) {
+      if (pokemons[i].data.name.includes(name)) {
+        filteredPokemons.push(pokemons[i]);
+      }
+    }
+    setPokemons(filteredPokemons);
+  };
+
+  return (
+    <div>
+      <Navbar pokemonFilter={pokemonFilter} />
+      <Container maxWidth="false">
+        <Grid container spacing={3}>
+          {pokemons.length === 0 ? <Skeletons/> : 
+          pokemons.map((pokemon, key) => (
+            <Grid item xs={12} sm={4} md={2} key={key}>
+              <PokemonCard
+                name={pokemon.data.name}
+                image={pokemon.data.sprites.front_default}
+                types={pokemon.data.types}
+              />
+            </Grid>
+          ))}
+          
+          
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default PokemonList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,64 +1,10 @@
-import { Grid } from "@mui/material";
-import { Container } from "@mui/system";
-import React from "react";
-import Navbar from "../components/Navbar";
-import PokemonCard from "../components/PokemonCard";
-import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
-import { Skeletons } from "../components/Skeletons";
+import React from 'react'
+import PokemonList from '../components/PokemonList'
 
 const Home = () => {
-  const [pokemons, setPokemons] = useState([]);
-  useEffect(() => {
-    getPokemons();
-  }, []);
-
-  const getPokemons = () => {
-    var endpoints = [];
-    for (var i = 1; i < 151; i++) {
-      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
-    }
-    axios
-      .all(endpoints.map((endpoint) => axios.get(endpoint)))
-      .then((res) => setPokemons(res))
-      .catch((err) => console.log(err));
-  };
-
-  const pokemonFilter = (name) => {
-    var filteredPokemons = [];
-    if (name === "") {
-      getPokemons();
-    }
-    for (var i in pokemons) {
-      if (pokemons[i].data.name.includes(name)) {
-        filteredPokemons.push(pokemons[i]);
-      }
-    }
-    setPokemons(filteredPokemons);
-  };
-
   return (
-    <div>
-      <Navbar pokemonFilter={pokemonFilter} />
-      <Container maxWidth="false">
-        <Grid container spacing={3}>
-          {pokemons.length === 0 ? <Skeletons/> : 
-          pokemons.map((pokemon, key) => (
-            <Grid item xs={12} sm={4} md={2} key={key}>
-              <PokemonCard
-                name={pokemon.data.name}
-                image={pokemon.data.sprites.front_default}
-                types={pokemon.data.types}
-              />
-            </Grid>
-          ))}
-          
-          
-        </Grid>
-      </Container>
-    </div>
-  );
-};
+    <PokemonList />
+  )
+}
 
-export default Home;
+export default Home
