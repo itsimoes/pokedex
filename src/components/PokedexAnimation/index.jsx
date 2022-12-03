8a9bca39b7bfa037d237cec16688c9d05d9b325f
itsimoes <2d.italo@gmail.com>
2022-12-03 16:50:34 -0300

    Component PokedexAnimation criado

---
+++ src/components/PokedexAnimation/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/system'
+import React from 'react'
+
+const PokedexAnimation = () => {
+  return (
+    <Container>
+        Pokedex Animation
+    </Container>
+  )
+}
+
+export default PokedexAnimation
